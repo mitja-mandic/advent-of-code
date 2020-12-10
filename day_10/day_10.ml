aa8513a17baf21ff518f686d06c3cafeb97c3187
Mitja Mandić <49949550+mitja-mandic@users.noreply.github.com>
2020-12-10 10:48:06 +0100

    prvi del narjen

---
+++ day_10/day_10.ml
@@ -0,0 +1,25 @@
+let day = "10"
+
+let input = "day_" ^ day ^"/day_" ^day^".in"
+
+let preberi_datoteko ime_datoteke =
+    let chan = open_in ime_datoteke in
+    let vsebina = really_input_string chan (in_channel_length chan) in
+    close_in chan;
+    vsebina
+
+let int_list vsebina_datoteke = 
+    let string_sez = String.split_on_char '\n' vsebina_datoteke in
+        List.map int_of_string string_sez
+
+let uredi seznam = List.sort compare seznam
+
+let prestej_razlike seznam =
+    let rec aux seznam ena tri prvi = match seznam with
+        | x :: y :: xs -> if y - x = 1 then aux (y::xs) (ena+1) tri x
+                        else if y - x = 3 then aux (y::xs) ena (tri+1) x else failwith "ojoj"
+        | x :: [] -> if x - prvi = 1 then aux [] (ena+1) tri x else aux [] ena (tri+1) x 
+        | [] ->  (tri+1) * ena in
+    aux seznam 0 0 (List.hd seznam)
+
+let naloga1 datoteka = datoteka |> preberi_datoteko |> int_list |> uredi |> prestej_razlike |> string_of_int
